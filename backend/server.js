218317a1c0b15b083801db15b338667c83c4664e
Veronika <terekhovaveronikawork@gmail.com>
2026-03-31 23:02:53 +0300

    hopelatcommit

--- backend/server.js
+++ backend/server.js
@@ -358,7 +358,7 @@ app.get(/.*/, (req, res) => {
 });
 
 // ЗАПУСК СЕРВЕРА
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`=========================================`);
   console.log(`Сервер працює на: http://localhost:${PORT}`);
